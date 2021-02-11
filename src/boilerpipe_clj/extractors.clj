e10e612a6a684e375264c6fe995462fb4b9ddbc3
Avelino <t@avelino.xxx>
2021-02-11 07:54:54 -0300

    wirte get-images tests
    Signed-off-by: Avelino <t@avelino.xxx>

--- src/boilerpipe_clj/extractors.clj
+++ src/boilerpipe_clj/extractors.clj
@@ -3,10 +3,9 @@
              ArticleExtractor
              ArticleSentencesExtractor
              DefaultExtractor)
-             (de.l3s.boilerpipe.sax ImageExtractor)))
+           (de.l3s.boilerpipe.sax ImageExtractor)))
 
 (defonce article-extractor (ArticleExtractor/getInstance))
 (defonce default-extractor (DefaultExtractor/getInstance))
-(defonce image-extractor (ImageExtractor/INSTANCE))
-(defonce article-sentence-extractor
-  (ArticleSentencesExtractor/getInstance))
+(defonce image-extractor (ImageExtractor/getInstance))
+(defonce article-sentence-extractor (ArticleSentencesExtractor/getInstance))
--- test/boilerpipe_clj/core_test.clj
+++ test/boilerpipe_clj/core_test.clj
@@ -1,13 +1,15 @@
 (ns boilerpipe-clj.core-test
   (:require [clojure.test :refer :all]
+            [clojure.string :as string]
             [boilerpipe-clj.core :refer :all]
             [boilerpipe-clj.extractors :as ext]))
 
 (defonce test-article
-  (slurp "./test/boilerpipe_clj/resources/greenspun-test.html"))
+         (slurp "./test/boilerpipe_clj/resources/greenspun-test.html"))
 
 (def extractors [ext/default-extractor
                  ext/article-extractor
+                 ext/image-extractor
                  ext/article-sentence-extractor])
 
 (deftest get-text-extraction
@@ -15,11 +17,22 @@
             without a provided extractor"
     (let [res (get-text test-article)]
       (is (and
-           (not= res "")
-           (not= res nil)))))
+            (not= res "")
+            (not= res nil)))))
   (testing "All the available extractors extract something
-           from the article"
+            from the article"
     (doseq [ext extractors
             :let [res (get-text test-article ext)]]
       (is (and (not= res "")
                (not (nil? res)))))))
+
+(deftest get-images-extraction
+  (testing "get-images extracts something from an html
+            content without a provided extractor"
+    (let [res (get-images "https://www.nytimes.com/2021/02/10/us/politics/trump-senate-impeachment-trial.html")]
+      (= (count res) 2)
+      (is (and
+            (not= res "")
+            (not= res nil)))
+      (doseq [i res] (string/includes? (.getSrc i) "https://"))
+      (doseq [i res] (number? (.getWidth i))))))
